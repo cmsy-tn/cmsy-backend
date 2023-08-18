502967a3e0b946263678c24abff0dd08bfec9bcb
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-18 22:58:03 +0100

    placeholder controller for FAQ

--- src/Modules/f-a-q/f-a-q.controller.ts
+++ src/Modules/f-a-q/f-a-q.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('f-a-q')
-export class FAQController {}
+export class FAQController {
+
+    @Get()
+    fetchAllFAQ() {
+        return [];
+    }
+
+    @Post()
+    createFAQ(@Body() payload: any) {
+        return null;
+    }
+
+    @Put(':id')
+    updateFAQ(
+        @Body() payload: any,
+        @Param() id: string
+    ) {
+        return null;
+    }
+
+    @Delete(':id')
+    deleteFAQ(@Param() id: string) {
+        return null;
+    }
+}
