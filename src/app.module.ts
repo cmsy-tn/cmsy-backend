39e8f72418ab16464656afe52a5ba4889dc739b6
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-18 22:14:26 +0100

    using global config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConnectionConfiguration } from './common/data-source';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(ConnectionConfiguration),
   ],
   controllers: [],
