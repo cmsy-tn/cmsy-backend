6b5cee4ba17ac32c836e1c5503bc10ba91bf541f
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-18 22:16:00 +0100

    FAQ module boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConnectionConfiguration } from './common/data-source';
 import { ConfigModule } from '@nestjs/config';
+import { FAQModule } from './Modules/f-a-q/f-a-q.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(ConnectionConfiguration),
+    FAQModule,
   ],
   controllers: [],
   providers: []
