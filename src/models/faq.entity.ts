7748d47dfefb55749b897fd613c2d8bedbcd03eb
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-18 22:58:25 +0100

    FAQ entitiy

---
+++ src/models/faq.entity.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity } from "typeorm";
+import BASE__ENTITY from "./base.entity";
+
+@Entity('faq')
+export default class FAQ_ENTITY extends BASE__ENTITY {
+
+    @Column()
+    question: string;
+
+    @Column()
+    answer: string;
+}
