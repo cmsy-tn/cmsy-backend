db5aafd8a665c7485803f2cb17d54e6fc8a4dcba
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-25 17:52:37 +0100

    openApi | SWAGGER documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  // SWAGGER BOOTSTRAPPING
+  const config = new DocumentBuilder()
+    .setTitle('TEACHICA CMS API')
+    .setDescription('Official API documentation for TEACHICA CMS.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
