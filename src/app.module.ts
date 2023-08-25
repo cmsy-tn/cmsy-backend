b3102e42389ad36cd7148457516dda8214765291
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-25 17:43:13 +0100

    services module boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConnectionConfiguration } from './common/data-source';
 import { ConfigModule } from '@nestjs/config';
 import { FAQModule } from './Modules/f-a-q/f-a-q.module';
+import { ServicesModule } from './Modules/services/services.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(ConnectionConfiguration),
     FAQModule,
+    ServicesModule,
   ],
   controllers: [],
   providers: []
