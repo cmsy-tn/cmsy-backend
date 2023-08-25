23dcddfabbba55e64307d4db8f3115d8029bae08
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-25 17:44:29 +0100

    services controller initial tempalte

--- src/Modules/services/services.controller.ts
+++ src/Modules/services/services.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ServicesService } from './services.service';
 
 @Controller('services')
-export class ServicesController {}
+export class ServicesController {
+
+    constructor(private servicesService: ServicesService) { }
+
+    @Get()
+    fetchAllFAQ() {
+        return this.servicesService.fetchAllFAQ();
+    }
+
+    @Post()
+    createFAQ(@Body() payload: any[]) {
+        return this.servicesService.createFAQ(payload);
+    }
+
+    @Put(':id')
+    updateFAQ(
+        @Body() payload: any,
+        @Param() id: string
+    ) {
+        return this.servicesService.updateFAQ(payload, id);
+    }
+
+    @Delete(':id')
+    deleteFAQ(@Param() id: string) {
+        return this.servicesService.deleteFAQ(id);
+    }
+
+}
