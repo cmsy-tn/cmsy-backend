d3e7eeb6fc08a01d00089ecf5950161e60656ded
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-25 17:50:30 +0100

    service entity declaration in module

--- src/Modules/services/services.module.ts
+++ src/Modules/services/services.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServicesController } from './services.controller';
 import { ServicesService } from './services.service';
+import SERVICE_ENTITY from 'src/models/service.entity';
+
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SERVICE_ENTITY])],
   controllers: [ServicesController],
   providers: [ServicesService]
 })
-export class ServicesModule {}
+export class ServicesModule { }
