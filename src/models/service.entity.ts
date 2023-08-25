f9b8193da78d419311d5d8e4d6e087cc7e85a56a
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-25 17:47:40 +0100

    SERVICE ENTITY

---
+++ src/models/service.entity.ts
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+import { Column, Entity } from "typeorm";
+import BASE__ENTITY from "./base.entity";
+import FAQ_ENTITY from "./faq.entity";
+
+@Entity('SERVICE')
+export default class SERVICE_ENTITY extends BASE__ENTITY {
+
+    @Column()
+    title: string;
+
+    @Column({ type: 'text' })
+    description: string;
+
+    @Column()
+    call_to_action: string;
+
+    @Column()
+    subServices: string[];
+
+    @Column({ default: false })
+    service_has_faq: boolean;
+
+    @Column()
+    service_faqs: FAQ_ENTITY[];
+}
