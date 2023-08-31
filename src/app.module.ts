5d79019f5b53426c279e93cc1f37c0637e6a2045
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-08-31 02:13:35 +0100

    Blog module boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConnectionConfiguration } from './common/data-source';
 import { ConfigModule } from '@nestjs/config';
 import { FAQModule } from './Modules/f-a-q/f-a-q.module';
 import { ServicesModule } from './Modules/services/services.module';
+import { BlogModule } from './Modules/blog/blog.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ServicesModule } from './Modules/services/services.module';
     TypeOrmModule.forRootAsync(ConnectionConfiguration),
     FAQModule,
     ServicesModule,
+    BlogModule,
   ],
   controllers: [],
   providers: []
